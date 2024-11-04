0fcbe203bb615b7fa0de33673bfecb78a0691df6
maayan32 <maayanshira.02@gmail.com>
2024-11-04 16:42:51 +0200

    Updated processing and removed large files
    - Added the first files
    - Removed heavy file
    - Updated the processing logic in turn_to_vector.py
    - Zipped the output file for better handling
    
    i added the first files
    
    hi
    
    deleted heavy file
    
    zipped one file, added changes to the loading of the files to data frames.

--- turn_to_vector.py
+++ turn_to_vector.py
@@ -1,6 +1,16 @@
 import pandas as pd
-data_space =pd.read_csv('cas_offinder_output.txt',delimiter='\t', header=None)
-data_space.columns =[]
+import zipfile
+# Read the CSV file directly from the zip
+with zipfile.ZipFile('cas_offinder_output2.zip') as z:
+    with z.open('cas_offinder_output2.txt') as f:
+        data_space = pd.read_csv(f, delimiter=r'\s+', header=None)
+# create titles for each column
+data_space.columns =['target', 'chrom', 'take_down', 'take_down2', 'take_down3', 'chromStart', 'offtarget_sequence', 'strand', 'distance', 'name']
+# delete the unwanted colums
+data_space.drop(columns=['take_down', 'take_down2', 'take_down3'], inplace=True)
+# change the lowercase letters in the offtarget that represent the missmatches to uppercase
+data_space['offtarget_sequence'] = data_space['offtarget_sequence'].str.upper()
 changeseq_real_results =pd.read_excel('changeseq_final_results.xlsx')
 print(data_space.head(1))
+# print(data_space.shape)
 print(changeseq_real_results.head(1))
