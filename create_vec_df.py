f654c6bd5815a207cd3afcacf7d8143dfe04e0a6
maayan32 <maayanshira.02@gmail.com>
2024-11-05 18:29:44 +0200

    added embedding and turn to vector files

---
+++ create_vec_df.py
@@ -0,0 +1,6 @@
+import orginize_df
+import turn_sample_to_vector as ts
+final_data_df = orginize_df.proccess_data()[0]
+only_target_offtarget = orginize_df.proccess_data()[1]
+# Create a new DataFrame with a single column by applying the function to each row
+vectors_and_labels = only_target_offtarget.DataFrame(only_target_offtarget.apply(lambda row: ts.create_full_feature_vector(row['name'], row['offtarget_sequence']), axis=1), columns=['Embedding'])
